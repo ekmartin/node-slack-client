abfb3ef10065afe38e9dae6007f8a294241e68d6
Leah <leah@l12s.com>
2015-11-23 15:48:12 -0800

    Adds in test stubs for star_added and star_removed events

--- test/clients/rtm/client-events.js
+++ test/clients/rtm/client-events.js
@@ -175,8 +175,28 @@ describe('RTM API Event Handlers', function () {
         });
 
 
-        describe('stars_xxx`` events', function () {
+        describe('star_xxx`` events', function () {
+            describe('star_added', function() {
 
+                it('stars a message when a `star_added` message with a `message` property is received');
+                it('stars a file when a `star_added` message with a `message` property is received');
+                it('stars a file_comment when a `star_added` message with a `file_comment` property is received');
+                it('stars a channel when a `star_added` message with a `channel` property is received');
+                it('stars a DM when a `star_added` message with a `im` property is received');
+                it('stars a group when a `star_added` message with a `group` property is received');
+
+            });
+
+            describe('star_removed', function() {
+
+                it('unstars a message when a `star_removed` message with a `message` property is received');
+                it('unstars a file when a `star_removed` message with a `message` property is received');
+                it('unstars a file_comment when a `star_removed` message with a `file_comment` property is received');
+                it('unstars a channel when a `star_removed` message with a `channel` property is received');
+                it('unstars a DM when a `star_removed` message with a `im` property is received');
+                it('unstars a group when a `star_removed` message with a `group` property is received');
+
+            });
         });
 
 
