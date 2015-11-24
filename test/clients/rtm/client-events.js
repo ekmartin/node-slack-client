ace00e484750a7050c2c3ed9933e0f1e91d98d36
Leah <leah@l12s.com>
2015-11-23 17:57:38 -0800

    Gets the groups tests working and refactors those + channels tests to use common functions where possible

--- test/clients/rtm/client-events.js
+++ test/clients/rtm/client-events.js
@@ -17,6 +17,7 @@ var rtmStartFixture = require('../../fixtures/rtm.start');
 
 var ALICE_USER_ID = 'U0CJ5PC7L';
 var TEST_CHANNEL_ID = 'C0CJ25PDM';
+var TEST_GROUP_ID = 'G0CHZSXFW';
 
 
 describe('RTM API Event Handlers', function () {
@@ -35,50 +36,6 @@ describe('RTM API Event Handlers', function () {
     });
 
     describe('Parsed Events', function () {
-
-        var testBaseChannelMarked = function(event, baseChannelId) {
-            var dataStore = getMemoryDataStore();
-
-            var baseChannel = dataStore.getChannelGroupOrDMById(baseChannelId);
-            baseChannel.history.push({ts: 1});
-            baseChannel.history.push({ts: 2});
-
-            var originalUnreads = baseChannel.recalcUnreads();
-            expect(originalUnreads).to.equal(2);
-
-            clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
-            var newUnreads = baseChannel.recalcUnreads();
-
-            expect(newUnreads).to.equal(0);
-        };
-
-        describe('`im_xxx` events', function () {
-
-            var testDMOpenStatus = function (isOpen, event) {
-                var dataStore = getMemoryDataStore();
-                dataStore.getDMById(rtmStartFixture.ims[0].id).isOpen = isOpen;
-                clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
-
-                expect(dataStore.getDMById(rtmStartFixture.ims[0].id).isOpen).to.equal(isOpen);
-            };
-
-            it('sets isOpen to true on a DM channel when an `im_open` message is received', lodash.partial(testDMOpenStatus, true, RTM_EVENTS.IM_OPEN));
-
-            it('sets isOpen to false on a DM channel when an `im_close` message is received', lodash.partial(testDMOpenStatus, false, RTM_EVENTS.IM_CLOSE));
-
-            it('adds a new DM object when an `im_created` message is received', function() {
-                var dataStore = getMemoryDataStore();
-
-                clientEventHandlers['im_created'](dataStore, getRTMMessageFixture('im_created'));
-                var dmChannel = dataStore.getDMById('D0CHZQWNP');
-                expect(dmChannel).to.not.be.undefined;
-            });
-
-            it('marks the DM channel as read when an `im_marked` message is received', function() {
-                testBaseChannelMarked('im_marked', 'D0CHZQWNP');
-            });
-
-        });
 
         describe('`bot_xxx` events', function () {
 
@@ -100,75 +57,167 @@ describe('RTM API Event Handlers', function () {
 
         });
 
-        describe('`channel_xxx` events', function () {
+        describe('channel, group and DM events', function() {
 
-            var isArchivedChange = function (event, expected) {
+            var isArchivedChange = function(event, id, expected) {
                 var dataStore = getMemoryDataStore();
 
                 clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
-                var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
-                expect(channel.isArchived).to.equal(expected);
+                var baseChannel = dataStore.getChannelGroupOrDMById(id);
+                expect(baseChannel.isArchived).to.equal(expected);
             };
 
-            it('sets isArchived to true on a channel when a `channel_archive` message is received', function() {
-                isArchivedChange('channel_archive', true);
-            });
-
-            it('sets isArchived to false on a channel when a `channel_unarchive` message is received', function() {
-                isArchivedChange('channel_unarchive', false);
-            });
-
-            it('renames a channel when a `channel_rename` message is received', function() {
+            var testBaseChannelMarked = function(event, baseChannelId) {
                 var dataStore = getMemoryDataStore();
 
-                clientEventHandlers['channel_rename'](dataStore, getRTMMessageFixture('channel_rename'));
-                var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
-                expect(channel.name).to.equal('test-channel-rename');
-            });
+                var baseChannel = dataStore.getChannelGroupOrDMById(baseChannelId);
+                baseChannel.history.push({ts: 1});
+                baseChannel.history.push({ts: 2});
 
-            it('creates a new channel when a `channel_created` message is received', function() {
+                var originalUnreads = baseChannel.recalcUnreads();
+                expect(originalUnreads).to.equal(2);
+
+                clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
+                var newUnreads = baseChannel.recalcUnreads();
+
+                expect(newUnreads).to.equal(0);
+            };
+
+            var testBaseChannelRename = function(event, baseChannelId, expected) {
                 var dataStore = getMemoryDataStore();
 
-                clientEventHandlers['channel_created'](dataStore, getRTMMessageFixture('channel_created'));
-                var channel = dataStore.getChannelById('C0F3Q8LH5');
-                expect(channel).to.not.be.undefined;
-            });
+                clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
+                var baseChannel = dataStore.getChannelGroupOrDMById(baseChannelId);
+                expect(baseChannel.name).to.equal(expected);
+            };
 
-            it('deletes a channel when a `channel_deleted` message is received', function() {
+            var testBaseChannelJoined = function(event, baseChannelId, expectedUserId) {
                 var dataStore = getMemoryDataStore();
 
-                clientEventHandlers['channel_deleted'](dataStore, getRTMMessageFixture('channel_deleted'));
-                var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
-                expect(channel).to.be.undefined;
-            });
+                clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
+                var baseChannel = dataStore.getChannelGroupOrDMById(baseChannelId);
+                expect(baseChannel.members).to.have.length(2);
+                expect(baseChannel).to.have.deep.property('members[1]', expectedUserId);
+            };
 
-            it('creates a new channel object and replaces the old one when a `channel_joined` message is received', function() {
+            var testBaseChannelLeft = function(event, baseChannelId, expectedUserId) {
                 var dataStore = getMemoryDataStore();
 
-                clientEventHandlers['channel_joined'](dataStore, getRTMMessageFixture('channel_joined'));
-                var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
-                expect(channel.members).to.have.length(2);
-                expect(channel).to.have.deep.property('members[1]', 'U0F3LFX6K');
+                clientEventHandlers[event](ALICE_USER_ID, '', dataStore, getRTMMessageFixture(event));
+                var baseChannel = dataStore.getChannelGroupOrDMById(baseChannelId);
+                expect(baseChannel.members).to.not.contain(expectedUserId);
+
+                return baseChannel;
+            };
+
+            describe('`channel_xxx` events', function () {
+
+                it('sets isArchived to true on a channel when a `channel_archive` message is received', function() {
+                    isArchivedChange('channel_archive', TEST_CHANNEL_ID, true);
+                });
+
+                it('sets isArchived to false on a channel when a `channel_unarchive` message is received', function() {
+                    isArchivedChange('channel_unarchive', TEST_CHANNEL_ID, false);
+                });
+
+                it('renames a channel when a `channel_rename` message is received', function() {
+                    testBaseChannelRename('channel_rename', TEST_CHANNEL_ID, 'test-channel-rename');
+                });
+
+                it('creates a new channel when a `channel_created` message is received', function() {
+                    var dataStore = getMemoryDataStore();
+
+                    clientEventHandlers['channel_created'](dataStore, getRTMMessageFixture('channel_created'));
+                    var channel = dataStore.getChannelById('C0F3Q8LH5');
+                    expect(channel).to.not.be.undefined;
+                });
+
+                it('deletes a channel when a `channel_deleted` message is received', function() {
+                    var dataStore = getMemoryDataStore();
+
+                    clientEventHandlers['channel_deleted'](dataStore, getRTMMessageFixture('channel_deleted'));
+                    var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
+                    expect(channel).to.be.undefined;
+                });
+
+                it('creates a new channel object and replaces the old one when a `channel_joined` message is received', function() {
+                    testBaseChannelJoined('channel_joined', TEST_CHANNEL_ID, 'U0F3LFX6K');
+                });
+
+                it('removes the user from a channel when a `channel_left` message is received', function() {
+                    testBaseChannelLeft('channel_left', TEST_CHANNEL_ID, 'U0F3LFX6K');
+                });
+
+                it('marks the channel as read when a `channel_marked` message is received', function() {
+                    testBaseChannelMarked('channel_marked', TEST_CHANNEL_ID);
+                });
+
             });
 
-            it('removes the user from a channel when a `channel_left` message is received', function() {
-                var dataStore = getMemoryDataStore();
+            describe('`group_xxx` events', function () {
 
-                clientEventHandlers['channel_left'](ALICE_USER_ID, '', dataStore, getRTMMessageFixture('channel_left'));
-                var channel = dataStore.getChannelById(TEST_CHANNEL_ID);
-                expect(channel.members).to.not.contain(ALICE_USER_ID);
+                it('sets isArchived to true on a group when a `group_archive` message is received', function() {
+                    isArchivedChange('group_archive', TEST_GROUP_ID, true);
+                });
+
+                it('sets isArchived to false on a group when a `group_unarchive` message is received', function() {
+                    isArchivedChange('group_unarchive', TEST_GROUP_ID, false);
+                });
+
+                it('marks the group as read when a `group_marked` message is received', function() {
+                    testBaseChannelMarked('group_marked', TEST_GROUP_ID);
+                });
+
+                it('renames a group when a `group_rename` message is received', function() {
+                    testBaseChannelRename('group_rename', TEST_GROUP_ID, 'test-group-rename');
+                });
+
+                it('creates a new group object and replaces the old one when a `group_joined` message is received', function() {
+                    testBaseChannelJoined('group_joined', TEST_GROUP_ID, 'U0F3LFX6K');
+                });
+
+                describe('`group_left`', function() {
+                    it('removes the user from a group when a `group_left` message is received', function() {
+                        testBaseChannelLeft('group_left', TEST_GROUP_ID, 'U0F3LFX6K');
+                    });
+
+                    it('marks the group as archived when the last user leaves', function() {
+                        var group = testBaseChannelLeft('group_left', TEST_GROUP_ID, 'U0F3LFX6K');
+                        expect(group.isArchived).to.be.true;
+                    });
+                });
+
             });
-            
-            it('marks the channel as read when a `channel_marked` message is received', function() {
-                testBaseChannelMarked('channel_marked', TEST_CHANNEL_ID);
+
+            describe('`im_xxx` events', function () {
+
+                var testDMOpenStatus = function (isOpen, event) {
+                    var dataStore = getMemoryDataStore();
+                    dataStore.getDMById(rtmStartFixture.ims[0].id).isOpen = isOpen;
+                    clientEventHandlers[event](dataStore, getRTMMessageFixture(event));
+
+                    expect(dataStore.getDMById(rtmStartFixture.ims[0].id).isOpen).to.equal(isOpen);
+                };
+
+                it('sets isOpen to true on a DM channel when an `im_open` message is received', lodash.partial(testDMOpenStatus, true, RTM_EVENTS.IM_OPEN));
+
+                it('sets isOpen to false on a DM channel when an `im_close` message is received', lodash.partial(testDMOpenStatus, false, RTM_EVENTS.IM_CLOSE));
+
+                it('adds a new DM object when an `im_created` message is received', function() {
+                    var dataStore = getMemoryDataStore();
+
+                    clientEventHandlers['im_created'](dataStore, getRTMMessageFixture('im_created'));
+                    var dmChannel = dataStore.getDMById('D0CHZQWNP');
+                    expect(dmChannel).to.not.be.undefined;
+                });
+
+                it('marks the DM channel as read when an `im_marked` message is received', function() {
+                    testBaseChannelMarked('im_marked', 'D0CHZQWNP');
+                });
+
             });
 
         });
-
-        describe('`group_xxx` events', function () {
-            
-        });
-
 
         describe('`presence_xxx` events', function () {
 
